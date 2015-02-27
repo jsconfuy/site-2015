721b9b2c1ae436779578e7330a7f24238271a975
Pablo Ricco <pricco@gmail.com>
2015-02-27 00:38:24 -0200

    Fix issues.

--- public/js/lib/base.js
+++ public/js/lib/base.js
@@ -1,94 +1,33 @@
 ;(function() {
   'use strict';
 
-  // FIXME: Navbar
-  // var elSelector  = 'body > header',
-  //   element = document.querySelector(elSelector);
-  // var element;
-
-  // if( !element ) return true;
-
-  // var elHeight = 0,
-  //   elTop = 0,
-  //   dHeight = 0,
-  //   wHeight = 0,
-  //   wScrollCurrent = 0,
-  //   wScrollBefore = 0,
-  //   wScrollDiff = 0;
-
-  // window.addEventListener('scroll', function()
-  // {
-  //   elHeight    = element.offsetHeight;
-  //   dHeight      = document.body.offsetHeight;
-  //   wHeight      = window.innerHeight;
-  //   wScrollCurrent  = window.pageYOffset;
-  //   wScrollDiff    = wScrollBefore - wScrollCurrent;
-  //   elTop      = parseInt(window.getComputedStyle( element ).getPropertyValue('top')) + wScrollDiff;
-
-  //   if( wScrollCurrent <= 0 ) // scrolled to the very top; element sticks to the top
-  //     element.style.top = '0px';
-
-  //   else if( wScrollDiff > 0 ) // scrolled up; element slides in
-  //     element.style.top = ( elTop > 0 ? 0 : elTop ) + 'px';
-
-  //   else if( wScrollDiff < 0 ) // scrolled down
-  //   {
-  //     if( wScrollCurrent + wHeight >= dHeight - elHeight )  // scrolled to the very bottom; element slides in
-  //       element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
-
-  //     else // scrolled down; element slides out
-  //       element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px';
-  //   }
-
-  //   wScrollBefore = wScrollCurrent;
-  // });
-
-  // $(document).on('ready', function () {
-  //   var sticky = function () {
-  //     if ($(document).scrollTop() > $('body > header').offset().top) {
-  //       $('body > header').addClass('sticky');
-  //     } else {
-  //       $('body > header').removeClass('sticky');
-  //     }
-
-  //     // if ($(document).scrollTop() > $('.speakers').offset().top - 300) {
-  //     //   $('.speakers li:not(.active)').each(function (index) {
-  //     //     var $el = $(this);
-  //     //     window.setTimeout(function () {
-  //     //       $el.addClass('active');
-  //     //     }, index * 100);
-  //     //   });
-  //     // }
-
-  //     // if ($(document).scrollTop() > $('.sponsors').offset().top - 300) {
-  //     //   $('.sponsors li:not(.active)').each(function (index) {
-  //     //     var $el = $(this);
-  //     //     window.setTimeout(function () {
-  //     //       $el.addClass('active');
-  //     //     }, index * 100);
-  //     //   });
-  //     // }
-  //   };
-  //   var stickyid;
-  //   var stickyout = function() {
-  //     if ($(document).scrollTop() > $('body > header').offset().top) {
-  //       $('body > header').addClass('sticky');
-  //     }
-  //     clearTimeout(stickyid);
-  //     stickyid = setTimeout(sticky, 50);
-  //   };
-  //   $(window).on('resize', stickyout);
-  //   $(document).on('scroll', stickyout);
-  // });
-  //
+  // NavBar
+  $(".navbar-fixed-top").autoHidingNavbar({});
 
   // Map
-   var map = new google.maps.Map(
-     document.getElementById('map'),
-     {
-       center: new google.maps.LatLng(-34.397, 150.644)
-     }
-   );
+  var point = new google.maps.LatLng(-34.892589, -56.194638);
+  var map = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      center: point,
+      zoom: 15,
+      draggable: false,
+      scrollwheel: false,
+      panControl: false,
+      streetViewControl: false,
+    }
+  );
+  var marker = new google.maps.Marker({
+    position: point,
+    map: map,
+    icon: {
+      url: '/images/logo.png',
+      size: new google.maps.Size(32, 32),
+      scaledSize: new google.maps.Size(32, 32),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(0, 32),
+    }
+  });
 
   // Tickets
   var modal = $('#buy');
